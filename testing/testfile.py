2920dbdcf85ebcea637e126f911013f239708e5b
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2021-09-27 15:47:00 +0100

    worked out copying issue

--- testing/testfile.py
+++ testing/testfile.py
@@ -3,5 +3,12 @@ import os
 files = os.listdir('testing/originals/')
 original_path = 'testing/originals'
 for file in files:
+    new_path = 'testing/copied'
     filepath = os.path.join(original_path, file)
-    print(filepath) 
+    with open(filepath, 'r') as f:
+        lines = f.read()
+    new_path = os.path.join(new_path, file)
+    with open(new_path, 'w') as f1:
+        f1.write(lines)
+
+    
